5d3b1fb136bc4d58399a8ee2fd38ace17357e55e
RAMADAN <aramadan318@gmail.com>
2024-09-22 22:17:24 +0200

    Update githubService.js to use correct API endpoint for fetching user data

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
+// Base URL for GitHub API
+const BASE_URL = 'https://api.github.com/users/';
+
 // Function to fetch user data from GitHub API
 export const fetchUserData = async (username) => {
     try {
         // Make an API call to fetch user data based on the username
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+        const response = await axios.get(`${BASE_URL}${username}`);
         return response.data; // Return the user data
     } catch (err) {
-        throw err; // Throw an error if the request fails
+        // If an error occurs, throw it to be caught in the component
+        throw err;
     }
 };
